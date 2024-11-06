cb510539305245a4f7ed1ee574c17fe3ad3576a5
MURILO VIEIRA CAETANO <murilo.vcaetano@senacsp.edu.br>
2024-11-06 17:15:08 -0300

    Nesse primeiro comentario houve uma nova alteração na classe cliente não foi feita inteiramente porem as classes ja estão feitas.

--- SysTINSClass/Produto.cs
+++ SysTINSClass/Produto.cs
@@ -39,33 +39,6 @@ namespace SysTINSClass
         {
             var cmd = Banco.Abrir();
 
-            // cmd.CommandText = $"insert into usuarios values (0, '{Nome}', '{Email}', md5('{Senha}'), {Nivel.Id}, default);";
-
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "sp_produtos_insert";
-            cmd.Parameters.AddWithValue("spcod_barra", Codbarra);
-            cmd.Parameters.AddWithValue("spvalor_unit", Valor);
-            cmd.Parameters.AddWithValue("spestoque_minimo", Estoque);
-            cmd.Parameters.AddWithValue("spid", Id);
-            var dr = cmd.ExecuteReader();
-            if (dr.Read())
-            {
-                Id = dr.GetInt32(0);
-            }
-            cmd.Connection.Close();
-        }
-
-        public bool Atualizar()
-        {
-            var cmd = Banco.Abrir();
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "sp_produto_altera";
-            cmd.Parameters.AddWithValue("spid", Id);
-            cmd.Parameters.AddWithValue("spcod_barra", Codbarra);
-            cmd.Parameters.AddWithValue("spvalor_unit", Valor);
-            cmd.Parameters.AddWithValue("spestoque_minimo", Estoque);
-            return cmd.ExecuteNonQuery() > 0 ? true : false;
-        }
     }
 
 }
